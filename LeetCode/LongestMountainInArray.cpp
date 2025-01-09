98fa8af9308e705ea363b9b8c38aad8050fa731d
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-01-09 22:53:37 +0700

    finish problem

--- LeetCode/LongestMountainInArray.cpp
+++ LeetCode/LongestMountainInArray.cpp
@@ -1,57 +1,87 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
     vector<int> arr={0,1,2,3,4,5,4,3,2,1,0};
-    int n=arr.size();
-    int l = 0,cnt = 0;
-    bool up = true;
-
-    for(int i=1;i<n;i++)
+    arr = {2,1,4,7,3,2,5};
+    int n = arr.size();
+    int cnt = 0 ;
+    int l = 0,r = 0 ;
+    for(int i=1;i<n-1;i++)
     {
-        if(arr[i] > arr[i-1])
+        if(arr[i-1] < arr[i] && arr[i] > arr[i+1])
         {
-            if(up == true)
-            {
-                continue;
-            }
-            else
-            {
-                if(i-l >= 3)
-                {
-                    cnt = max(cnt, i-l);
-                }
-                l = i-1;
-                up = true;
-            }
+            l = i-1;
+            r = i+1;
+
+            while(l >= 0 && arr[l] < arr[l+1])l-=1;
+            while(r < n && arr[r-1] > arr[r]) r += 1;
+
+            cnt = max(cnt,r-l-1);
         }
-        else if(arr[i] < arr[i-1])
-        {
-            if(up == false)
-            {
-                continue;
-            }
-            else
-            {
-                up = false;
-            }
-        }
-        else
-        {
-            l = i;
-            up = true;
-        }
-        // cout << i -l  << endl;
     }
 
-
-    cout << cnt << " ";
-
-
-
-    return 0;
+    cout << cnt;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> arr={0,1,2,3,4,5,4,3,2,1,0};
+//     arr={2,1,4,7,3,2,5};
+//     int n=arr.size();
+//     int l = 0,cnt = 0;
+//     bool up = true;
+
+//     for(int i=1;i<n;i++)
+//     {
+//         if(arr[i] > arr[i-1])
+//         {
+//             if(up == true)
+//             {
+//                 continue;
+//             }
+//             else        // This condition is met when the slope is turn from down to up.
+//             {
+//                 if(i-l >= 3)
+//                 {
+//                     cnt = max(cnt, i-l);
+//                 }
+//                 l = i-1;
+//                 up = true;
+//             }
+//         }
+//         else if(arr[i] < arr[i-1])
+//         {
+//             if(up == false)
+//             {
+//                 if(i == n-1 && i-l >= 3) cnt = max(cnt,i-l+1);
+//                 continue;
+//             }
+//             else
+//             {
+//                 up = false;
+//             }
+//         }
+//         else
+//         {
+//             l = i;
+//             up = true;
+//         }
+//     }
+
+
+//     cout << cnt << " ";
+
+
+
+//     return 0;
+// }
