a14d6846884ecc6b83454eacc94344bc190df406
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-02-16 23:51:39 +0700

    finish

--- Grader/BionomialCoefficient.cpp
+++ Grader/BionomialCoefficient.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
-#define ll long long
-
+#include <vector>
 using namespace std;
+
+int binomialCoefficientDP(int n, int k, vector<vector<int>> &dp)
+{
+    if (k == 0 || k == n)
+        return 1;
+    if (dp[n][k] != -1)
+        return dp[n][k];
+
+    dp[n][k] = binomialCoefficientDP(n - 1, k - 1, dp) + binomialCoefficientDP(n - 1, k, dp);
+    return dp[n][k];
+}
 
 int main()
 {
     int n, k;
-    ll ans = 0;
+    cin >> n >> k;
 
-    cout << ans;
+    vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
+    cout << binomialCoefficientDP(n, k, dp) << endl;
+
     return 0;
 }
