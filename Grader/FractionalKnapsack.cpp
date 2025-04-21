27c557c877f2822c2b999dde80301b59a4971952
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-04-21 15:43:55 +0700

    more

---
+++ Grader/FractionalKnapsack.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    double w;
+    ll n;
+    cin >> w >> n;
+    vector<double> tv(n), tw(n);
+    double sum = 0.0;
+    priority_queue<pair<double, double>, vector<pair<double, double>>> maxPQ;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tv[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tw[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        maxPQ.push({tv[i] / tw[i], i});
+    }
+
+    while (w > 0 && !pq.empty())
+    {
+        auto [_, idx] = maxPQ.top();
+        maxPQ.pop();
+
+        if (w >= tw[idx])
+        {
+            w -= tw[idx];
+            sum += tv[idx];
+        }
+        else if (w < tw[idx])
+        {
+            sum += (w / tw[idx]) * tv[idx];
+            w -= tw[idx];
+        }
+    }
+
+    std::cout << std::fixed
+              << std::setprecision(4)
+              << sum
+              << '\n';
+
+    return 0;
+}
